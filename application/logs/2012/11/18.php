aeb81e9696360b79cd9471924470027157315bc7
Nicholas Curtis <nich.curtis@gmail.com>
2012-11-18 18:40:37 -0800

    added module deputy

---
+++ application/logs/2012/11/18.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2012-11-18 20:39:39 --- ERROR: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'deputy' at 'MODPATH/deputy' ~ SYSPATH/classes/kohana/core.php [ 550 ]
+2012-11-18 20:39:39 --- STRACE: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'deputy' at 'MODPATH/deputy' ~ SYSPATH/classes/kohana/core.php [ 550 ]
+--
+#0 /Users/ncurtis/GitHub/nichcurtis/MessWithRainee/application/modules.php(20): Kohana_Core::modules(Array)
+#1 /Users/ncurtis/GitHub/nichcurtis/MessWithRainee/application/bootstrap.php(107): require_once('/Users/ncurtis/...')
+#2 /Users/ncurtis/GitHub/nichcurtis/MessWithRainee/index.php(96): require('/Users/ncurtis/...')
+#3 {main}
+2012-11-18 20:39:39 --- ERROR: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'deputy' at 'MODPATH/deputy' ~ SYSPATH/classes/kohana/core.php [ 550 ]
+2012-11-18 20:39:39 --- STRACE: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'deputy' at 'MODPATH/deputy' ~ SYSPATH/classes/kohana/core.php [ 550 ]
+--
+#0 /Users/ncurtis/GitHub/nichcurtis/MessWithRainee/application/modules.php(20): Kohana_Core::modules(Array)
+#1 /Users/ncurtis/GitHub/nichcurtis/MessWithRainee/application/bootstrap.php(107): require_once('/Users/ncurtis/...')
+#2 /Users/ncurtis/GitHub/nichcurtis/MessWithRainee/index.php(96): require('/Users/ncurtis/...')
+#3 {main}
